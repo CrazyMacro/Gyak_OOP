09f7756d8d044e88bb1a4cf91cd1ebd4bafc86f6
Dániel Kuknyó <Daniel.Kuknyo.78@unibge.hu>
2019-04-02 11:58:26 +0200

    sudoku interface commit 2

---
+++ SudokuSolver/SudokuSolver/Solver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Solver
+    {
+        internal Form1 mainForm;
+        HashSet<int> Possible = new HashSet<int>();
+        int[,] tomb = new int[9, 9]; 
+
+        public Solver(Form1 mainForm)
+        {
+            this.mainForm = mainForm;
+            for(int i=0; i<9; i++)
+            {
+                for(int j=0; j<9; j++)
+                {
+                    string s = "TB" + i + j;
+
+                }
+            }
+        }
+
+        void PossibleNum(int toAdd)
+        {
+            
+        }
+    }
+}
