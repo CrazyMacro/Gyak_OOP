498ff4941213e185a4ec12f006616a701b6c16fa
Dániel Kuknyó <Daniel.Kuknyo.78@unibge.hu>
2019-04-23 12:06:53 +0200

    solver method updated

--- SudokuSolver/SudokuSolver/Solver.cs
+++ SudokuSolver/SudokuSolver/Solver.cs
@@ -91,7 +91,7 @@ namespace SudokuSolver
                         {
                             current.Value = current.PossibleNums.Single();
                             added = current.PossibleNums.Single();
-                            valueAddedRowColumnArea(added, i, j);
+                            valueAdded(tomb[i, j]);
                         }
                     }
                 }
@@ -230,24 +230,6 @@ namespace SudokuSolver
             foreach(Cell current in currentArea)
             {
                 current.RemovePossibleNum(c.Value);
-            }
-        }
-
-        void valueAddedRowColumnArea(int n, int x, int y)
-        {
-            for(int i=0; i<9; i++)
-            {
-                for(int j=0; j<9; j++)
-                {
-                    if(i == x && j == y)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        valueAdded(tomb[x, y]);
-                    }
-                }
             }
         }
     }
